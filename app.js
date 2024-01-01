21f51dacbde408a1e8a5479479473a8b95e00f4b
Bhavik2209 <bhavikrohit22@gmail.com>
2024-01-01 13:46:37 +0530

    add files

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const port=3000;
 const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
 const app=express();
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));    
 app.use(express.static("public"));
 
+let posts=[];
+
 app.get("/",(req,res)=>{
     res.render("home",{
-        para:homeStartingContent
+        para:homeStartingContent,
+        posts:posts
     })
+
 })
 app.get("/about",(req,res)=>{
     res.render("about",{
@@ -31,8 +34,13 @@ app.get("/compose",(req,res)=>{
     res.render("compose");
 });
 app.post("/compose",(req,res)=>{
-    const blog=req.body.inputBlog;
-    console.log(blog);
+    
+    const post={
+        title:req.body.inputBlog,
+        content:req.body.postBody
+    };
+    posts.push(post);
+    res.redirect("/");
 });
 
 app.listen(port,()=>{
