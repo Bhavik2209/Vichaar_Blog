d0ced385f1da5cfb6f65461529fa6b7ca2801d2b
Bhavik2209 <bhavikrohit22@gmail.com>
2024-01-03 11:30:46 +0530

    update

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
 const port = 3000;
-const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 const app = express();
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,20 +15,15 @@ let posts = [];
 
 app.get("/", (req, res) => {
     res.render("home", {
-        para: homeStartingContent,
         posts: posts
     });
 
 });
 app.get("/about", (req, res) => {
-    res.render("about", {
-        aboutC: aboutContent
-    });
+    res.render("about");
 });
 app.get("/contact", (req, res) => {
-    res.render("contact", {
-        contactC: contactContent
-    });
+    res.render("contact");
 });
 app.get("/compose", (req, res) => {
     res.render("compose");
@@ -42,6 +36,9 @@ app.post("/compose", (req, res) => {
     };
     posts.push(post);
     res.redirect("/");
+});
+app.post("/",(req,res)=>{
+    res.redirect("/compose");
 });
 
 app.get("/posts/:postName", (req, res) => {
